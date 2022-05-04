6ab6dd5ee3417b701a97d78927660c62b0b105bc
ahmed pc <ahmedatefadelaly@gmail.com>
2022-05-05 01:24:44 +0200

    first commita

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./navbar.css";
+const Navbar = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="nav">
+          <div className="logo"></div>
+          <div className="about"></div>
+          <div className="designs"></div>
+          <div className="contact"></div>
+          <button className="btn"></button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
